7f4cc99aaeb89e2975a6d9206d3241808d2a33da
Sophia Ho <swho@andrew.cmu.edu>
2021-10-04 11:34:26 -0400

    lab updates

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
 function addNewList() {
-    alert('hello world!');
+    let todoLists = document.getElementById("todo-lists")
+    let btn = document.getElementById("new-list-button")
+    const newList = document.createElement("DIV")
+    newList.classList.add("list")
+    todoLists.insertBefore(newList, btn)
 }
 
 function addListItem() {
-    console.log("hello world!")
+    let list = document.getElementById("grocery-list")
+    let itemInput = document.getElementById("new-list-item")
+    let newItem = document.createElement("li")
+    newItem.appendChild(document.createTextNode(itemInput.value))
+    list.appendChild(newItem);
 }
 
+function deleteListItem(item) {
+    // remove li element (item) from ol element (item.parentNode)
+    item.parentNode.removeChild(item);
+}
+
+function completeListItem(item) {
+    if (item.checked) { // true if the input checkbox is checked
+        item.parentNode.style.textDecoration = "line-through";
+        // in css, this would be: "text-decoration: line-through"
+    } else {
+        item.parentNode.style.textDecoration = "none";
+        // in css, this would be: "text-decoration: none"
+    }
+}
 
